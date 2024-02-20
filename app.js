b3d4c7c951f858f5d28a72f26f2c4807454c0fbb
MZeeshanMalik <m121zeeshan@gmail.com>
2024-02-20 01:31:35 -0800

    fixed bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const globalErrorHandler = require('./Controller/errorController')
+const globalErrorHandler = require('./controller/errorController')
 const bodyParser = require('body-parser')
 const userRouter = require('./Router/userRouter')
 const orderRouter = require('./Router/orderRoutes')
